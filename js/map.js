7de7e9cdfc4d4f6524c964b14f00599da36ed634
w1liu <liuwenbolwb@gmail.com>
2015-07-06 13:11:42 -0700

    a

--- js/map.js
+++ js/map.js
@@ -1,15 +1,17 @@
 // Function to draw your map
-var drawMap() = function() {
+var drawMap = function() {
 
   // Create map and set viewd
- 
+  var map = L.map('container');
+  map.setView([47.6550,-122.3080],8);
 
+  
+  var layer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+  
+  layer.addTo(map);
   // Create an tile layer variable using the appropriate url
-
-  // Add the layer to your map
- 
-
   // Execute your function to get data
+  getData();
  
 }
 
@@ -17,16 +19,36 @@ var drawMap() = function() {
 var getData = function() {
 
   // Execute an AJAX request to get the data in data/response.js
-
-
+  var data;
+  $.ajax({
+  		  url:'data/response.json';
+		  type: "get",
+    success:function(dat) {
+       data = dat
+    } 
+   dataType:"json"
+}) 
+ /*  customBuild(data);
   // When your request is successful, call your customBuild function
 
 }
-
 // Do something creative with the data here!  
-var customBuild = function() {
-
-  
+var customBuild = function(data) {
+		 data.map(function(crime) {
+		   var marker = new L.circle(
+		   [crime.lat,crime.lng],50,{
+		   color:'red',
+		   opacity:.5
+		   }).addTo(map);
+		   var text = crime['Armed or Unarmed']+' , '+crime['Hit or Killed'];
+		   marker.bindPopup(text);
+		   marker.on('mouseover', function (e) {
+           this.openPopup();
+           });
+           marker.on('mouseout', function (e) {
+            this.closePopup();
+        });
+		   
+	}
 }
-
-
+*/
